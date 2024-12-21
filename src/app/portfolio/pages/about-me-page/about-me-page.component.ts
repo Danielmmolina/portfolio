5ec6757bc8fa93440806ba3c349fe650dc20d609
danielmolina22 <danielmolinamoran18@gmail.com>
2024-12-21 20:45:29 -0300

    Abou-me Page

--- src/app/portfolio/pages/about-me-page/about-me-page.component.ts
+++ src/app/portfolio/pages/about-me-page/about-me-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { BtnComponent } from '../../../shared/components/btn/btn.component';
 
 @Component({
   selector: 'app-about-me-page',
   standalone: true,
-  imports: [],
+  imports: [BtnComponent],
   templateUrl: './about-me-page.component.html',
-  styleUrl: './about-me-page.component.css'
+  styleUrl: './about-me-page.component.css',
 })
-export default class AboutMePageComponent {
-
-}
+export default class AboutMePageComponent {}
